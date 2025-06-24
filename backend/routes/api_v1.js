0a4c539f6cdbf3cab4c143c0d6a75014438eb545
Jack Campbell <jackcampbell@meta.com>
2025-06-23 17:24:01 -0700

    Added index file and inital routing to api and non-profit-employee

---
+++ backend/routes/api_v1.js
@@ -0,0 +1,30 @@
+// Routes starting with /api/v1
+import { PrismaClient } from '../generated/prisma/client.js';
+import express from 'express'
+import {getNonProfit} from '../utils/nonprofit_utils.js'
+
+// Import routes
+import nonprofitRoutes from './nonprofit-employee.js'
+
+const prisma = new PrismaClient()
+const apiRouter = express.Router()
+
+// Default route for /api/v1
+apiRouter.get('/', (req, res) => {
+    res.send('Welcome to API V1!');
+});
+
+
+// START nonprofit-employee
+
+// Route for /api/v1/nonprofit-employee
+apiRouter.get('/nonprofit-employee', (req, res) => {
+    // DEBUG This endpoint should never return anything but is currently located here for debugging purposes
+    res.send('Non Profit employee endpoint!');
+});
+
+// Route for /api/v1/nonprofit-employee/:nonprofitname
+apiRouter.use('/nonprofit-employee/:nonprofitname',getNonProfit, nonprofitRoutes)
+
+
+export default apiRouter
