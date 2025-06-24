3f942e17e5f405319e617f9a80131b1cb418cf2a
Jack Campbell <jackbcampbell15@gmail.com>
2025-06-24 15:03:13 -0700

    Refactor var names

--- backend/routes/nonprofit.js
+++ backend/routes/nonprofit.js
@@ -21,12 +21,12 @@ nonprofitRouter.get("/all", async (req, res) => {
 // Get one nonprofit by name
 nonprofitRouter.get("/:nonprofitname", async (req, res) => {
   const { nonprofitname } = req.params;
-  const findCard = await prisma.nonprofit.findUnique({
+  const findNonProfit = await prisma.nonprofit.findUnique({
     where: {
       name: nonprofitname,
     },
   });
-  res.status(200).json(findCard);
+  res.status(200).json(findNonProfit);
 });
 
 // Add a new nonprofit by name
@@ -34,12 +34,12 @@ nonprofitRouter.post("/add", async (req, res) => {
   const nonProfitData = req.body;
   const name = nonProfitData.name;
 
-  const findCards = await prisma.nonprofit.findUnique({
+  const findNonProfits = await prisma.nonprofit.findUnique({
     where: {
       name: name,
     },
   });
-  if (!findCards) {
+  if (!findNonProfits) {
     const createNonProfit = await prisma.nonprofit.create({
       data: nonProfitData,
     });
