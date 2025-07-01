21dd9c917ec547feb816bf51ce97f419fd440298
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-01 15:31:30 -0700

    Used default data

--- frontend/src/utils/fetch/nonProfitFetchUtils.js
+++ frontend/src/utils/fetch/nonProfitFetchUtils.js
@@ -1,15 +1,10 @@
 import { getNonProfit } from "#utils/pathUtils";
+import { statsDefault } from "#default-data/nonProfitDefaultData";
 const nonProfitLink = import.meta.env.VITE_BACKEND_API;
 
 export async function fetchNonProfitStats(after) {
   let nonProfit = getNonProfit();
-  const statsDefault = {
-    servicesNumber: { name: "Services #", key: 2 },
-    servicesOffered: { name: "Services offered", key: 30 },
-    popularZipCode: { name: "Popular Zip Codes", key: 40 },
-    servicesOffered2: { name: "Cost", key: 80 },
-  };
-  after(statsDefault);
+  after(statsDefault); // Temp until frontend connects to backend
   // await fetch(`${nonProfit}${id}/stats`)
   //     .then((response) => {
   //       if (!response.ok) {
