fc88c008de22ca6f99e656d51ef81907ad4c977e
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-01 15:20:35 -0700

    Update docs

--- frontend/src/utils/pathUtils.js
+++ frontend/src/utils/pathUtils.js
@@ -1,6 +1,7 @@
 import { elementType } from "prop-types";
 import { useLocation } from "react-router";
 
+// An enum for all of the pages to allow for easy navigation and prevent typos
 export const NpPages = Object.freeze({
   DASHBOARD: "dashboard",
   EDITSERVICE: "editservice",
@@ -11,6 +12,10 @@ export const NpPages = Object.freeze({
   SIGNUP: "signup",
 });
 
+/**
+ * Gets the last part of the url bar to help with highlighting the correct nav bar item
+ * @returns The last part of the url bar
+ */
 export function getLocation() {
   let location = useLocation().pathname;
   let allLocations = location.split("/");
