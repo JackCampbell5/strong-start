abc64053bca75f3e052d26ec7bad024e8dae3606
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-01 16:37:59 -0700

    Non Profit Selector is now own component

---
+++ frontend/src/components/NonProfitSelector/NonProfitSelector.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./NonProfitSelector.css";
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import { nonprofitAllDefault } from "#default-data/nonProfitDefaultData";
+import { fetchNonProfitList } from "#fetch/nonProfitFetchUtils";
+
+function NonProfitSelector({ errorText, setErrorText }) {
+  {
+    // Add query params when the dropdown is changed
+    const navigate = useNavigate();
+    function addNonProfit(e) {
+      let val = e.target.value;
+      const params = new URLSearchParams(location.search);
+      if (val === "unselected") {
+        params.delete("nonprofit");
+      } else {
+        console.log(val);
+        params.set("nonprofit", val); // Set your query parameter here
+        setErrorText("");
+      }
+      navigate(`?${params.toString()}`); // Update the URL with the new query parameter
+    }
+
+    // The default value of the selector should include th query params from the url if they exist
+    function setSelectorFromParams() {
+      const params = new URLSearchParams(location.search);
+      const nonprofit = params.get("nonprofit");
+      if (nonprofit !== null) {
+        return nonprofit;
+      }
+    }
+
+    // Fetch the nonprofit list from the backend
+    const [nonprofitList, setNonprofitList] = useState(nonprofitAllDefault);
+    function setNonprofitListHelper(data) {
+      if (!data[0].key === "unselected") {
+        data = [{ id: "unselected", text: "Not Selected" }, ...data];
+      }
+      setNonprofitList(data);
+    }
+
+    useEffect(() => {
+      fetchNonProfitList(setNonprofitListHelper);
+    }, []);
+    return (
+      <div className="NonProfitSelector">
+        <h2>What Non Profit are you looking for?</h2>
+        <div className="select-nonprofit">
+          <select
+            onChange={addNonProfit}
+            defaultValue={setSelectorFromParams()}
+          >
+            {nonprofitList.map((val) => {
+              return (
+                <option key={val.id} value={val.id}>
+                  {val.text}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <p className="errorText">{errorText}</p>
+      </div>
+    );
+  }
+}
+
+NonProfitSelector.propTypes = {
+  // data: PropTypes.func.isRequired,
+};
+
+export default NonProfitSelector;
--- frontend/src/components/SubPageSelect/SubPageSelect.jsx
+++ frontend/src/components/SubPageSelect/SubPageSelect.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./SubPageSelect.css";
-import { useEffect, useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import IconComp from "#components/IconComp/IconComp";
 import { IoEarth } from "react-icons/io5";
 import { FaHandsHelping } from "react-icons/fa";
-
-// Helper functions
-import { fetchNonProfitList } from "#fetch/nonProfitFetchUtils";
-import { nonprofitAllDefault } from "#default-data/nonProfitDefaultData";
-
 import PropTypes, { checkPropTypes } from "prop-types";
+
+// Other components
+import NonProfitSelector from "#components/NonProfitSelector/NonProfitSelector";
 
 function SubPageSelect({ changePage }) {
   // Make sure a nonprofit is selected before switching pages
@@ -26,45 +24,9 @@ function SubPageSelect({ changePage }) {
     }
   }
 
-  // Add query params when the dropdown is changed
-  const navigate = useNavigate();
-  function addNonProfit(e) {
-    let val = e.target.value;
-    const params = new URLSearchParams(location.search);
-    if (val === "unselected") {
-      params.delete("nonprofit");
-    } else {
-      console.log(val);
-      params.set("nonprofit", val); // Set your query parameter here
-      setErrorText("");
-    }
-    navigate(`?${params.toString()}`); // Update the URL with the new query parameter
-  }
-
-  // The default value of the selector should include th query params from the url if they exist
-  function setSelectorFromParams() {
-    const params = new URLSearchParams(location.search);
-    const nonprofit = params.get("nonprofit");
-    if (nonprofit !== null) {
-      return nonprofit;
-    }
-  }
-
-  // Fetch the nonprofit list from the backend
-  const [nonprofitList, setNonprofitList] = useState(nonprofitAllDefault);
-  function setNonprofitListHelper(data) {
-    if (!data[0].key === "unselected") {
-      data = [{ id: "unselected", text: "Not Selected" }, ...data];
-    }
-    setNonprofitList(data);
-  }
-
   // Error handling
   const [errorText, setErrorText] = useState("");
 
-  useEffect(() => {
-    fetchNonProfitList(setNonprofitListHelper);
-  }, []);
   return (
     <div className="SubPageSelect">
       <IconComp />
@@ -81,21 +43,10 @@ function SubPageSelect({ changePage }) {
             <br /> <strong>Non-Profit-</strong> A non-profit employee looking to
             edit/manage services
           </p>
-          <h2>What Non Profit are you looking for?</h2>
-          <div className="select-nonprofit">
-            <select
-              onChange={addNonProfit}
-              defaultValue={setSelectorFromParams()}
-            >
-              {nonprofitList.map((val) => {
-                return (
-                  <option key={val.id} value={val.id}>
-                    {val.text}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
+          <NonProfitSelector
+            errorText={errorText}
+            setErrorText={setErrorText}
+          />
           <h2>What Role Are you?</h2>
           <div className="select-div">
             <div
@@ -113,7 +64,6 @@ function SubPageSelect({ changePage }) {
               Non-Profit
             </div>
           </div>
-          <p className="errorText">{errorText}</p>
         </div>
       </main>
     </div>
