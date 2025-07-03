b99d14fccedc951743b7d874aaed64e4eb0899a4
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-03 14:20:53 -0700

    Help Page

--- frontend/src/pages/refugee/Help/Help.jsx
+++ frontend/src/pages/refugee/Help/Help.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './Help.css'
-import PropTypes from 'prop-types';
+import "./Help.css";
+import PropTypes from "prop-types";
 
 function Help({}) {
   return (
     <div className="Help">
-      <h3>Help</h3>
+      <h1>Help</h1>
+      <p className="helpText">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque
+        possimus asperiores repudiandae deserunt impedit dolore illum. Eos,
+        consequatur. Nostrum, eaque consequatur. Nesciunt id iusto possimus
+        repudiandae autem in mollitia rem.
+      </p>
     </div>
   );
 }
 
 Help.propTypes = {
-    // data: PropTypes.func.isRequired,
+  // data: PropTypes.func.isRequired,
 };
 
 export default Help;
