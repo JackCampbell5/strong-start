470c771acd5ba080a7715d145eaff4421bfd9d95
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-07 10:27:28 -0700

    Updated after so login worked properly

--- frontend/src/utils/fetch/nonprofitEmployeeFetchUtils.js
+++ frontend/src/utils/fetch/nonprofitEmployeeFetchUtils.js
@@ -4,11 +4,10 @@ const serviceLink = import.meta.env.VITE_BACKEND_API + "/nonprofit-employee";
 /**
  * Creates a new service for a nonprofit
  * @param {object} info - The info to post to the backend
- * @param {*} after  - Function to call with data fetched
  */
-export async function loginNonprofitEmployee(username, password, after) {
+export async function loginNonprofitEmployee(username, password) {
   let nonProfit = getNonProfit();
-  after({ success: true, message: "Logged in" });
+  return { success: true, message: "Logged in" };
   //   // Temp until frontend connects to backend
   //   await fetch(`${serviceLink}/${nonProfit}/login`, {
   //     method: "POST",
@@ -23,13 +22,13 @@ export async function loginNonprofitEmployee(username, password, after) {
   //     })
   //     .then((data) => {
   //       // Update now that success
-  //       after(data);
+  //       return data;
   //     })
   //     .catch((error) => {
   //       // Handle error
   //       console.error("Error fetching boards:", error);
   //       // Return more info on the error
-  //       after();
+  //       return data;
   //     });
 }
 /**
