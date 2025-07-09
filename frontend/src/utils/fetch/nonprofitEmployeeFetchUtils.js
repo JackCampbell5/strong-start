f31ecbebee9c7d93cdd7ee97f77981c8d3ea1faf
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-09 15:30:35 -0700

    Login method for fetch nonprofit

--- frontend/src/utils/fetch/nonprofitEmployeeFetchUtils.js
+++ frontend/src/utils/fetch/nonprofitEmployeeFetchUtils.js
@@ -1,4 +1,5 @@
 import { getNonProfit } from "#utils/pathUtils";
+import { errorReturn, successReturn, MyHTTPError } from "#utils/httpUtils";
 const serviceLink =
   import.meta.env.VITE_BACKEND_API + "/api/v1/nonprofit-employee";
 
@@ -6,30 +7,31 @@ const serviceLink =
  * Creates a new service for a nonprofit
  * @param {object} info - The info to post to the backend
  */
-export async function loginNonprofitEmployee(username, password) {
-  return { success: true, message: "Logged in" };
-  //   // Temp until frontend connects to backend
-  //   await fetch(`${serviceLink}/${nonProfit}/login`, {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify(info),
-  //   })
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error(`HTTP error! status: ${response.status}`);
-  //       }
-  //       return response.json(); // Parse JSON data from the response
-  //     })
-  //     .then((data) => {
-  //       // Update now that success
-  //       return data;
-  //     })
-  //     .catch((error) => {
-  //       // Handle error
-  //       console.error("Error fetching boards:", error);
-  //       // Return more info on the error
-  //       return data;
-  //     });
+export async function loginNonprofitEmployee(nonprofit, username, password) {
+  let data = { data: { username: username, password: password } };
+  return await fetch(`${serviceLink}/${nonprofit}/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then(async (response) => {
+      if (!response.ok) {
+        const errorText = await response.text(); // Read the response as text
+        throw new MyHTTPError(response.status, errorText);
+      }
+      return response.json(); // Parse JSON data from the response
+    })
+    .then((data) => {
+      // Update now that success
+      return successReturn("Welcome " + data.username);
+    })
+    .catch((error) => {
+      // Handle error
+      console.error("Error fetching boards:", error);
+      // Return more info on the error
+      return errorReturn(error);
+    });
 }
 
 /**
