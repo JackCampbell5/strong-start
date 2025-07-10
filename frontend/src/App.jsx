b9017467b42106ced92236123b2f250474b76a04
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-10 14:32:24 -0700

    Bug fix to allow page to load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 // Node Module Imports
-import { BrowserRouter as Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useNavigate, useLocation } from "react-router";
 
 // Local Imports
