8c75f3cfaf99fc0e7682ef1ce60cf6a6d4a35e5c
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-10 09:47:50 -0700

    Earlier reveal to employee when creating a new service that they should log in

--- frontend/src/pages/nonprofit/NewService/NewService.jsx
+++ frontend/src/pages/nonprofit/NewService/NewService.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./NewService.css";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import "./NewService.css";
+
+// Components
 import EditService from "#components/EditService/EditService";
 
+//util functions
+import { testLoginNonprofitEmployee } from "#fetch/nonprofitEmployeeFetchUtils";
+import { getNonProfit } from "#utils/pathUtils";
+
 function NewService() {
+  const nonprofit = getNonProfit();
+  const [errorText, setErrorText] = useState("");
+
+  useEffect(() => {
+    testLoginNonprofitEmployee(nonprofit).then((result) => {
+      if (result.valid) {
+        if (!result.data) {
+          setErrorText("No user signed in, please sign in to add a service");
+        }
+      } else {
+        setErrorText(result.error);
+      }
+    });
+  }, []);
   return (
     <div className="NewService">
       <h1>Create a New Service</h1>
+      <p className="errorText">{errorText}</p>
       <EditService />
     </div>
   );
