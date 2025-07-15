d0f914acc81d4afe1b387ba265b0733b28b03a6d
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-15 11:35:16 -0700

    Service validation before adding

--- backend/utils/service-utils.js
+++ backend/utils/service-utils.js
@@ -1,6 +1,11 @@
 // Node Module Imports
 import { PrismaClient } from "#prisma/client.js";
 
+// Local Imports
+import formatAddress from "#utils/search/address-utils.js";
+import { getAndValidateDate } from "#search/search-services.js";
+import { errorReturn, successReturn } from "./validate-utils.js";
+
 const prisma = new PrismaClient();
 
 /**
@@ -43,5 +48,43 @@ export async function checkServiceId(id, nonprofit, next) {
     return resultData !== null;
   } catch (e) {
     next(e);
+  }
+}
+
+export async function validateServiceData(serviceData, nonprofit = null) {
+  let updatedService = { ...serviceData };
+  let errorMessage = "";
+
+  // Format address and add
+  let addressInfo = await formatAddress(updatedService.address, nonprofit);
+
+  if (!addressInfo.valid) {
+    errorMessage += addressInfo.errorMessage + ", ";
+  } else {
+    updatedService.addressInfo = addressInfo.data;
+    updatedService.address = addressInfo.data.formattedAddress;
+  }
+
+  // Format the addresses as an array
+  if (updatedService.language) {
+    updatedService.language = updatedService.language.split(",").map((lang) => {
+      return lang.trim().toLowerCase();
+    });
+  } else {
+    updatedService.language = [];
+  }
+
+  // Validate the date
+  let date = getAndValidateDate(updatedService.date_needed);
+  if (!date.valid) {
+    errorMessage += date.error + ", ";
+  } else {
+    updatedService.date_needed = date.data.toISOString();
+  }
+
+  if (errorMessage.length > 0) {
+    return errorReturn(errorMessage.slice(0, -2));
+  } else {
+    return successReturn(updatedService);
   }
 }
