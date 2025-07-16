eb90fee0036802933e78b1986445a94a46be9454
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-16 13:30:42 -0700

    Dont print params that do not exist

--- frontend/src/components/Service/Service.jsx
+++ frontend/src/components/Service/Service.jsx
@@ -13,11 +13,11 @@ function Service({ data }) {
   const noShow = ["id", "name", "addressInfo", "routeLink", "nonprofit_ID"];
   return (
     <div className="Service">
-      <h3>{data.name}</h3>
+      {data.name ? <h3>{data.name}</h3> : null}
       {Object.entries(data).map((obj) => {
         const key = obj[0]; // gets the key from obj.entries
         const info = obj[1]; // gets the string stored at the given key from obj.entries
-        return !noShow.includes(key) ? (
+        return !noShow.includes(key) && info ? (
           <p className="serviceParam" key={key} href={data.routeLink}>
             <strong>{key}:</strong>{" "}
             {key === "routeLength" ? (
