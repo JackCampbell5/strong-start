acd03f21802f5945d12ed12b38da2a4f91c2b6e5
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-16 12:05:38 -0700

    Create Recommend and create all params

--- frontend/src/pages/nonprofit/Recommend/Recommend.jsx
+++ frontend/src/pages/nonprofit/Recommend/Recommend.jsx
@@ -1,18 +1,55 @@
+// Node Module Imports
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from 'prop-types';
-import './Recommend.css'
+import { useState } from "react";
 
-function Recommend({}) {
+// Local imports
+import "./Recommend.css";
+// Other components
+import ServiceList from "#components/ServiceList/ServiceList";
+import LoadingButton from "#components/LoadingButton/LoadingButton";
+// util functions
+import { fetchAllServices } from "#fetch/serviceFetchUtils";
+import { getNonProfit } from "#utils/pathUtils";
+
+function Recommend() {
+  // Constant Variables
+  const nonprofit = getNonProfit();
+
+  // State Variables
+  const [searchResults, setSearchResults] = useState([]);
+  const [errorText, setErrorText] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  function getRecommendations() {
+    setLoading(true);
+    fetchRecommendations(nonprofit).then(fetchRecommendationsCallback);
+  }
+  function fetchRecommendationsCallback(result) {
+    setLoading(false);
+    if (!result.valid) {
+      setErrorText(result.error);
+    } else {
+      setSearchResults(result.data);
+    }
+  }
+
   return (
     <div className="Recommend">
       <h3>Recommend</h3>
+      {searchResults.length === 0 ? (
+        <LoadingButton
+          onClick={getRecommendations}
+          loading={loading}
+          text={"Recommend"}
+          fit={true}
+        />
+      ) : (
+        <ServiceList services={searchResults} />
+      )}
+      <p className="errorText">{errorText}</p>
     </div>
   );
 }
-
-Recommend.propTypes = {
-    // data: PropTypes.func.isRequired,
-};
 
 export default Recommend;
