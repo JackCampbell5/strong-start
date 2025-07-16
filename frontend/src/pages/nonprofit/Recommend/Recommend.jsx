cd608c3ef86389e9c196635600cd20ea37eb2dd5
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-16 11:50:51 -0700

    Add Recommend component

---
+++ frontend/src/pages/nonprofit/Recommend/Recommend.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from 'prop-types';
+import './Recommend.css'
+
+function Recommend({}) {
+  return (
+    <div className="Recommend">
+      <h3>Recommend</h3>
+    </div>
+  );
+}
+
+Recommend.propTypes = {
+    // data: PropTypes.func.isRequired,
+};
+
+export default Recommend;
