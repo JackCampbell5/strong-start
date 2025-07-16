064c83e32c814700fe4fcb53cf99189a2d6899f4
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-16 14:37:05 -0700

    When a service is successfully added remove from recommendation list

--- frontend/src/components/RecList/RecList.jsx
+++ frontend/src/components/RecList/RecList.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 // Local imports
 import "./RecList.css";
@@ -10,14 +11,28 @@ import RecService from "#components/RecService/RecService";
 
 function RecList({ data }) {
   let num = 0;
+  const [serviceList, setServiceList] = useState(data);
+
+  function doneAdded(id) {
+    let updatedData = [...data];
+    for (let i = 0; i < updatedData.length; i++) {
+      if (updatedData[i].id === id) {
+        updatedData.splice(i, 1);
+        break;
+      }
+    }
+    setServiceList(updatedData);
+  }
   return (
     <div className="RecList">
       <h3>RecList</h3>
       <div className="allRecs">
-        {data
-          ? data.map((obj) => {
+        {serviceList
+          ? serviceList.map((obj) => {
               num++;
-              return <RecService key={obj.id} data={obj} />;
+              return (
+                <RecService key={obj.id} data={obj} doneAdded={doneAdded} />
+              );
             })
           : null}
       </div>
--- frontend/src/components/RecService/RecService.jsx
+++ frontend/src/components/RecService/RecService.jsx
@@ -2,23 +2,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
+import { useState } from "react";
 import { MdAddHome } from "react-icons/md";
 
 // Local Imports
 import "./RecService.css";
 // Other Components
 import Service from "#components/Service/Service";
+import EditService from "#components/EditService/EditService";
 
-function RecService({ data }) {
+function RecService({ data, doneAdded }) {
   const name = data?.name ?? "No Name Provided";
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  function successAdded() {
+    doneAdded(data.id);
+  }
 
   return (
     <div className="RecService">
       <div className="serviceHeader">
         <h3 className="serviceTitle">{name}</h3>
-        <MdAddHome />
+        <MdAddHome onClick={() => setIsExpanded((prev) => !prev)} />
       </div>
       <Service data={{ ...data, name: undefined }} />
+      {isExpanded && <EditService data={data} onValidAdd={successAdded} />}
     </div>
   );
 }
