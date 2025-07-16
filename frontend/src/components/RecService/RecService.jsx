badff3ee9ff9451b2c16ed74d085f6bf242a3e5c
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-16 13:30:55 -0700

    Top bar has title and edit abilities

--- frontend/src/components/RecService/RecService.jsx
+++ frontend/src/components/RecService/RecService.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
+import { MdAddHome } from "react-icons/md";
 
 // Local Imports
 import "./RecService.css";
@@ -9,12 +10,15 @@ import "./RecService.css";
 import Service from "#components/Service/Service";
 
 function RecService({ data }) {
+  const name = data?.name ?? "No Name Provided";
+
   return (
     <div className="RecService">
       <div className="serviceHeader">
-        <h3>RecService</h3>
+        <h3 className="serviceTitle">{name}</h3>
+        <MdAddHome />
       </div>
-      <Service data={data} />
+      <Service data={{ ...data, name: undefined }} />
     </div>
   );
 }
