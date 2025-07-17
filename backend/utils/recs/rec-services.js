8bb3c5552b7e57c73b27d5a66927dc3dc9b6da8b
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-17 12:28:23 -0700

    removeServiceDuplicates implemented

--- backend/utils/recs/rec-services.js
+++ backend/utils/recs/rec-services.js
@@ -20,8 +20,57 @@ export default async function recServices(nonprofit) {
     return errorReturn(result.error);
   }
 
+  const apiServices = result.data.places;
+
+  // Remove Duplicates Services Already in Database
+  const servicesNoDups = await removeServiceDuplicates(apiServices, nonprofit);
+
   // Reformat the services
-  const reformattedServices = reformatServices(result.data.places);
+  const reformattedServices = reformatServices(servicesNoDups);
 
   return successReturn(reformattedServices);
+}
+
+/**
+ * Removes duplicate services from the API
+ * + Compares name, zipcode, and address to see if the service already exists in the database
+ * + If it does, it is removed from the array
+ *
+ * @param {Array} apiServices - The services from the API
+ * @param {object} nonprofit - The nonprofit to find the duplicate services in
+ * @returns Array - The services without database duplicates
+ */
+async function removeServiceDuplicates(apiServices, nonprofit) {
+  if (!apiServices) return [];
+  let existedServices = await prisma.service.findMany({
+    where: {
+      nonprofit_ID: nonprofit.id,
+    },
+  });
+  const existedZipcodes = getAllOfKey(existedServices, "zipcode");
+  const existedNames = getAllOfKey(existedServices, "name");
+  const existedAddresses = getAllOfKey(existedServices, "address");
+
+  let servicesNoDups = [];
+  for (const service of apiServices) {
+    if (
+      !existedZipcodes.includes(service.zipcode) &&
+      !existedNames.includes(service?.displayName?.text) &&
+      !existedAddresses.includes(service.formattedAddress)
+    ) {
+      servicesNoDups.push(service);
+    }
+  }
+  return servicesNoDups;
+}
+
+/**
+ * Gets all of the values of a key in an array of objects
+ * @param {Array} services - The services to get the key of
+ * @param {string} key - The key to get
+ * @returns An array of the values of that key
+ */
+function getAllOfKey(services, key) {
+  let allKey = services.map((service) => service[key]);
+  return allKey.filter((item) => item);
 }
