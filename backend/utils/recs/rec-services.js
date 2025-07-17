76c710d2ca912f36c1bea21f913268aa3bfc26e4
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-17 10:32:04 -0700

    Add

--- backend/utils/recs/rec-services.js
+++ backend/utils/recs/rec-services.js
@@ -4,12 +4,24 @@ const prisma = new PrismaClient();
 
 // Local Imports
 import { errorReturn, successReturn } from "#utils/validate-utils.js";
+import servicesNearby from "#recs/services-nearby.js";
+import { reformatServices } from "#recs/rec-utils.js";
 
+/**
+ * Takes a nonprofit and finds additional services nearby
+ * + Uses the google places API to find nearby nonprofits and reformats the data and returns it
+ * @param {object} nonprofit - The nonprofit to find additional services for
+ * @returns {Array} - The reformatted services nearby
+ */
 export default async function recServices(nonprofit) {
-  const foundServices = await prisma.service.findMany({
-    where: {
-      nonprofit_ID: nonprofit.id,
-    },
-  });
-  return successReturn(foundServices);
+  // Find nearby services
+  let result = await servicesNearby(nonprofit);
+  if (!result.valid) {
+    return errorReturn(result.error);
+  }
+
+  // Reformat the services
+  const reformattedServices = reformatServices(result.data.places);
+
+  return successReturn(reformattedServices);
 }
