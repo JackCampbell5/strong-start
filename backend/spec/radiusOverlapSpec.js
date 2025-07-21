dc0983be4638af4d6e705d09efbca21de4f88132
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-21 12:49:51 -0700

    Tests for new functions

---
+++ backend/spec/radiusOverlapSpec.js
@@ -0,0 +1,85 @@
+// Node Module Imports
+import seedrandom from "seedrandom";
+
+// Local Module Imports
+import { radiusOverlap } from "#search/dist-utils.js";
+
+describe("radiusOverlap function", () => {
+  let rng = seedrandom(0);
+  function randomInRange(min, max) {
+    return rng() * (max - min) + min;
+  }
+  const mainRange = {
+    low: { longitude: -122, latitude: 45 },
+    high: { longitude: -121, latitude: 47 },
+  };
+  beforeAll(() => {
+    const seed = jasmine.getEnv().seed;
+    if (seed) {
+      rng = seedrandom(seed);
+    }
+  });
+  it("Not At All", () => {
+    const lat = randomInRange(-120, 170);
+    const long = randomInRange(48, 80);
+    const check1 = {
+      low: {
+        longitude: lat,
+        latitude: long,
+      },
+      high: {
+        longitude: lat + randomInRange(1, 10),
+        latitude: long + randomInRange(1, 10),
+      },
+    };
+
+    expect(radiusOverlap(mainRange, check1)).toBe(false);
+  });
+  it("Lat correct ", () => {
+    const lat = randomInRange(-121, -121.5);
+    const long = randomInRange(48, 80);
+    const check1 = {
+      low: {
+        longitude: lat,
+        latitude: long,
+      },
+      high: {
+        longitude: lat + 0.5,
+        latitude: long + randomInRange(1, 10),
+      },
+    };
+
+    expect(radiusOverlap(mainRange, check1)).toBe(false);
+  });
+  it("All correct ", () => {
+    const lat = randomInRange(-121, -121.5);
+    const long = randomInRange(45, 46.5);
+    const check1 = {
+      low: {
+        longitude: lat,
+        latitude: long,
+      },
+      high: {
+        longitude: lat + 0.5,
+        latitude: long + 0.5,
+      },
+    };
+
+    expect(radiusOverlap(mainRange, check1)).toBe(true);
+  });
+  it("On the broder ", () => {
+    const lat = -122;
+    const long = 47;
+    const check1 = {
+      low: {
+        longitude: lat,
+        latitude: long,
+      },
+      high: {
+        longitude: lat + randomInRange(1, 10),
+        latitude: long + randomInRange(1, 10),
+      },
+    };
+    expect(radiusOverlap(mainRange, check1)).toBe(true);
+  });
+});
---
+++ backend/spec/serviceInRadiusSpec.js
@@ -0,0 +1,43 @@
+// Node Module Imports
+import seedrandom from "seedrandom";
+
+// Local Module Imports
+import { serviceInRadius } from "#search/dist-utils.js";
+
+describe("serviceInRadius function", () => {
+  let rng = seedrandom(0);
+  function randomInRange(min, max) {
+    return rng() * (max - min) + min;
+  }
+  const mainRange = {
+    low: { longitude: -122, latitude: 45 },
+    high: { longitude: -121, latitude: 47 },
+  };
+  beforeAll(() => {
+    const seed = jasmine.getEnv().seed;
+    if (seed) {
+      rng = seedrandom(seed);
+    }
+  });
+  it("Inside", () => {
+    let service = { longitude: -121.5, latitude: 46.5 };
+    expect(serviceInRadius(mainRange, service)).toBe(true);
+  });
+  it("Outside lat", () => {
+    let service = { longitude: -121.5, latitude: 48.5 };
+    expect(serviceInRadius(mainRange, service)).toBe(false);
+  });
+
+  it("Outside long", () => {
+    let service = { longitude: -123.5, latitude: 46.5 };
+    expect(serviceInRadius(mainRange, service)).toBe(false);
+  });
+  it("Outside both", () => {
+    let service = { longitude: -123.5, latitude: 48.5 };
+    expect(serviceInRadius(mainRange, service)).toBe(false);
+  });
+  it("On Border ", () => {
+    let service = { longitude: -122, latitude: 47 };
+    expect(serviceInRadius(mainRange, service)).toBe(true);
+  });
+});
