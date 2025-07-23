4bd8a53a64e71007c0071e2e7bdcc3838dcb99ca
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-23 12:46:32 -0700

    Encode recommended services hours correctly

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,2 +1,11 @@
 export const nonprofitRadius = 50; // Radius in miles to search around the nonprofit
-export const googleAPIMaxPageNumber = 3; // Max number of pages returned by Google API search
+export const googleAPIMaxPageNumber = 1; // Max number of pages returned by Google API search
+export const googleToMeDaysConversion = {
+  0: 6,
+  1: 0,
+  2: 1,
+  3: 2,
+  4: 3,
+  5: 4,
+  6: 5,
+};
