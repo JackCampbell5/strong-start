392416a346b1bad454adf6335964dc1823b5652d
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-24 11:39:19 -0700

    Update variable value to reverse change

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,5 +1,5 @@
 export const nonprofitRadius = 50; // Radius in miles to search around the nonprofit
-export const googleAPIMaxPageNumber = 1; // Max number of pages returned by Google API search
+export const googleAPIMaxPageNumber = 3; // Max number of pages returned by Google API search
 export const googleToMeDaysConversion = {
   0: 6,
   1: 0,
