95cca64e6c7def3cf491ec2e57d4c6ba58f8e3f8
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-25 10:02:44 -0700

    Update footer with additional information

--- frontend/src/components/FooterNonProfit/FooterNonProfit.jsx
+++ frontend/src/components/FooterNonProfit/FooterNonProfit.jsx
@@ -1,16 +1,35 @@
 // Node Module Imports
-import React from "react";
+import React, { use, useEffect } from "react";
 import PropTypes from "prop-types";
 
 // Local Imports
 import "./FooterNonProfit.css";
 
-function FooterNonProfit() {
+function FooterNonProfit({ data }) {
+  const name = data.name;
+  const email = data.email;
+  const phone = data.phone;
+  const address = data.address;
+  const website = data.website;
+  const logo = data.logo;
+
   return (
     <div className="FooterNonProfit">
+      <p>{name}</p>
+      <p>
+        {phone} {email}
+      </p>
+      <p>{address}</p>
+      <a href={website} target="_blank">
+        {website}
+      </a>
       <p>Contact: StrongStart@gmail.com with any questions</p>
     </div>
   );
 }
+
+FooterNonProfit.propTypes = {
+  data: PropTypes.object.isRequired,
+};
 
 export default FooterNonProfit;
