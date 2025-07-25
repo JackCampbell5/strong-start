7ed80dc1a210eed2ea9d6fa8828a2cda1d63d36d
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-25 16:09:25 -0700

    Update recService for editing

--- frontend/src/components/RecService/RecService.jsx
+++ frontend/src/components/RecService/RecService.jsx
@@ -3,38 +3,54 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import { useState } from "react";
-import { MdAddHome } from "react-icons/md";
+import { MdAddHome, MdCreate } from "react-icons/md";
 
 // Local Imports
 import "./RecService.css";
 // Other Components
 import Service from "#components/Service/Service";
 import EditService from "#components/EditService/EditService";
+import { getNonProfit } from "#utils/pathUtils";
 
 function RecService({ data, serviceAddedSuccessfully }) {
+  // Constant Variables
   const name = data?.name ?? "No Name Provided";
+  const nonprofit = getNonProfit();
+  const existingInCurrentDatabase = nonprofit === data.nonprofit_ID;
+  // State Variables
   const [isExpanded, setIsExpanded] = useState(false);
 
   /**
    * If the employee successfully adds a service, we want to remove it from the recommended list so we call the function passed in from the parent list component
    */
-  function onValidAdd() {
-    serviceAddedSuccessfully(data.id);
+  function onValidAdd(validAddData) {
+    if (!existingInCurrentDatabase) {
+      serviceAddedSuccessfully(data.id);
+    } else {
+      serviceAddedSuccessfully(data.id, validAddData, true);
+      setIsExpanded(false);
+    }
   }
 
   return (
     <div className="RecService">
       <div className="serviceHeader">
         <h3 className="serviceTitle">{name}</h3>
-        <MdAddHome
+        <div
           title="Edit/Add Service to Database"
           onClick={() => setIsExpanded((prev) => !prev)}
-        />
+        >
+          {existingInCurrentDatabase ? <MdCreate /> : <MdAddHome />}
+        </div>
       </div>
       <Service inputData={{ ...data, name: undefined }} />
       {isExpanded && (
         <div className="editTopDivider">
-          <EditService inputData={data} onValidAdd={onValidAdd} />
+          <EditService
+            inputData={data}
+            onValidAdd={onValidAdd}
+            serviceID={existingInCurrentDatabase ? data.id : null}
+          />
         </div>
       )}
     </div>
