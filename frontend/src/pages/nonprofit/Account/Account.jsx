ac1a036b8318a60f9594eb7121869006949d8b66
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-29 10:51:28 -0700

    Get current account settings to validate them to setup for fetch request

--- frontend/src/pages/nonprofit/Account/Account.jsx
+++ frontend/src/pages/nonprofit/Account/Account.jsx
@@ -1,17 +1,91 @@
 // Node Module Imports
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 // Local Imports
 import "./Account.css";
 // Other Components
-import AccountInfoChange from "#components/AccountInfoChange/AccountInfoChange";
+import AccountInfoChange from "#components/AccountInfoCheck/AccountInfoCheck";
+import Register from "#components/Register/Register";
+import { getNonProfit } from "#utils/pathUtils";
+import { checkEmployeeLoginStatus } from "#utils/fetch/nonprofitEmployeeFetchUtils";
+import AccountInfoCheck from "#components/AccountInfoCheck/AccountInfoCheck";
+import { errorReturn, successReturn } from "#utils/httpUtils";
+import LoadingButton from "#components/LoadingButton/LoadingButton";
 
-function Account({}) {
+function Account({ nav }) {
+  // Constant Variables
+  const nonprofit = getNonProfit();
+  const [validAccount, setValidAccount] = useState(false);
+  // State Variables
+  const [userChecked, setUserChecked] = useState(false);
+  const [initUsername, setInitUsername] = useState("");
+  const [initEmail, setInitEmail] = useState("");
+  const [errorText, setErrorText] = useState("");
+
+  async function registerFetch(_, accountInfo) {
+    let reqBody = {};
+    if (accountInfo.username && accountInfo.username !== initUsername) {
+      reqBody.username = accountInfo.username;
+    }
+    if (accountInfo.email && accountInfo.email !== initEmail) {
+      reqBody.email = accountInfo.email;
+    }
+    if (accountInfo.password) {
+      reqBody.password = accountInfo.password;
+    }
+    if (Object.keys(reqBody).length === 0) {
+      console.log("No changes made to account as no fields were changed");
+      return errorReturn(
+        "No changes made to account as no fields were changed"
+      );
+    }
+    // Todo: Add account editing
+    return successReturn("Account edited");
+  }
+
+  function LoginStatusCallback(result) {
+    console.log("LoginStatusCallback", result);
+    if (result.valid) {
+      const data = result.data;
+      setInitUsername(data.username);
+      setInitEmail(data.email);
+      setValidAccount(true);
+    } else {
+      setErrorText(result.error);
+    }
+  }
+
+  useEffect(() => {
+    checkEmployeeLoginStatus(nonprofit).then(LoginStatusCallback);
+  }, []);
   return (
     <div className="Account">
-      <h3>Account</h3>
-      <AccountInfoChange />
+      <h1>Account Settings</h1>
+      {validAccount ? (
+        <div className="validAccount">
+          <button onClick={() => setUserChecked((prev) => !prev)}>
+            Check Account Info
+          </button>
+          {userChecked ? (
+            <div className="changeAccountInfo">
+              <p>Change whatever params you want</p>
+              <Register
+                setLoggedIn={() => {}}
+                nav={nav}
+                registerFetch={registerFetch}
+                defaults={{ username: initUsername, email: initEmail }}
+                newAccount={false}
+              />
+            </div>
+          ) : (
+            <AccountInfoCheck setUserChecked={setUserChecked} />
+          )}
+        </div>
+      ) : (
+        <LoadingButton loading={true} />
+      )}
+      <p className="errorText">{errorText}</p>
     </div>
   );
 }
