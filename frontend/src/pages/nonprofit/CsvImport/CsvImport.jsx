ac2f111b2f7629d1950c490d762cfaba67caf80d
Jack Campbell <jackbcampbell15@gmail.com>
2025-07-30 15:55:10 -0700

    CSV Import Component

---
+++ frontend/src/pages/nonprofit/CsvImport/CsvImport.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import "./CsvImport.css";
+import { uploadCsvServices } from "#utils/fetch/serviceFetchUtils";
+import { getNonProfit } from "#utils/pathUtils";
+import RecList from "#components/RecList/RecList";
+
+function CsvImport({}) {
+  const nonprofit = getNonProfit();
+  const [fileInput, setFileInput] = useState(null);
+  const [errorText, setErrorText] = useState("");
+  const [successText, setSuccessText] = useState("");
+  const [importedServices, setImportedServices] = useState([]);
+  const handleChange = (event) => {
+    setFileInput(event.target.files[0]);
+  };
+
+  function uploadCsvCallback(result) {
+    console.log(result);
+    setErrorText("");
+    setSuccessText("");
+    if (result.valid) {
+      // setSuccessText(result.data);
+      setImportedServices(result.data);
+    } else {
+      setErrorText(result.error);
+    }
+  }
+  function handleSubmit() {
+    const formData = new FormData();
+    formData.append("file", fileInput);
+    uploadCsvServices(formData, nonprofit).then(uploadCsvCallback);
+  }
+  return (
+    <div className="CsvImport">
+      <h3>CsvImport</h3>
+      <div>
+        <input
+          type="file"
+          name="file"
+          icon="file text outline"
+          iconPosition="left"
+          label="Upload CSV"
+          labelPosition="right"
+          placeholder="UploadCSV..."
+          onChange={handleChange}
+        />
+      </div>
+      <button onClick={handleSubmit} className="submitButton">
+        Submit
+      </button>
+      <div className="errorText">{errorText}</div>
+      <div className="successText">{successText}</div>
+      {importedServices.length !== 0 && (
+        <RecList
+          data={importedServices}
+          exportData={setImportedServices}
+          editorOnly={true}
+        />
+      )}
+    </div>
+  );
+}
+
+CsvImport.propTypes = {
+  // data: PropTypes.func.isRequired,
+};
+
+export default CsvImport;
